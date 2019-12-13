0e2299de1964a17e0cc385909e72c78c696b4dc5
Mac In Life <marie.ange.coco@gmail.com>
2019-12-13 12:10:03 +0100

    Change title

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var sassMiddleware = require("node-sass-middleware");
 //var fetch = require("node-fetch");
+var session = require("express-session");
+var flash = require("connect-flash");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "twig");
@@ -28,6 +29,16 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 }
+  })
+);
+app.use(flash());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
-//apport de compte user
 const fs = require("fs");
+//apport de compte user
 let rawdata = fs.readFileSync("./users.json");
 const users = JSON.parse(rawdata);
+
+var flash = require("connect-flash");
+//const users = JSON.parse(rawdata);
+// const request = require("request-json");
+// const data = __dirname + "/../data/users.json";
+// const client = request.createClient("https://jsonplaceholder.typicode.com/");
+
+// client.get("users", function(err, res, body) {
+//   fs.writeFile(data, JSON.stringify(body), err => {});
+// });
+
+// let rawdata = fs.readFileSync(data);
+// const users = JSON.parse(rawdata);
 
 var router = express();
 
@@ -11,11 +24,11 @@ const Joi = require("@hapi/joi");
 var methodOverride = require("method-override");
 
 // override with the X-HTTP-Method-Override header in the request
-router.use(methodOverride("X-HTTP-Method-Override"));
+router.use(methodOverride("_method"));
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
-  res.render("users/index", { users });
+  res.render("users/index", { users, error: req.flash("error") });
 });
 
 //GET - router/users/:id - Get a user
@@ -31,7 +44,7 @@ router.get("/:id", function(req, res, next) {
   res.render("users/users-id", { user });
 
   //PUT - router/users/:id : Update a user
-  router.post("/:id", (req, res) => {
+  router.put("/:id", (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
     if (!user) {
       return res.status(404).send("L'utilisateur n'existe pas ! Modifié");
@@ -49,8 +62,11 @@ router.get("/:id", function(req, res, next) {
         .required()
     });
     const validation = schema.validate(req.body);
+    //const { error } = validateCustomer(req.body); //result.error
     if (validation.error) {
-      return res.status(400).send(validation.error.details[0].message);
+      //return res.status(400).send(validation.error.details[0].message);
+      req.flash("error", validation.error.details[0].message);
+      return res.redirect("/users");
     }
 
     //User Modification
@@ -58,13 +74,15 @@ router.get("/:id", function(req, res, next) {
       (user.username = req.body.username),
       (user.email = req.body.email);
 
-    //res.render(userId);
-    //res.send(user);
-    res.redirect("/users");
+    return res.redirect("/users");
+    // if (error) {
+    //
+    //   return res.redirect("/users", { user, error: req.flash("error") });
+    // }
   });
 
   //DELETE - router/users/:id : Delete a user
-  router.post("/del/:id", (req, res) => {
+  router.delete("/del/:id", (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
     if (!user) {
       return res.status(404).send("L'utilisateur n'existe pas ! Suppression");
@@ -100,9 +118,10 @@ router.post("/users/index", (req, res) => {
     return res.status(400).send(validation.error.details[0].message);
   }
 
+  let max = Math.max(...users.map(user => user.id));
   //User Création
   const user = {
-    id: users.length + 1,
+    id: max + 1,
     name: req.body.name,
     username: req.body.username,
     email: req.body.email
@@ -113,5 +132,38 @@ router.post("/users/index", (req, res) => {
   return res.redirect("/users");
   //res.render(user);
 });
+
+// //PUT - router/users/:id : Update a user
+// router.put("/:id", (req, res) => {
+//   const user = users.find(u => u.id === parseInt(req.params.id));
+//   if (!user) {
+//     return res.status(404).send("L'utilisateur n'existe pas ! Modifié");
+//   }
+//   //Vérification des données requises
+//   const schema = Joi.object({
+//     name: Joi.string()
+//       .min(3)
+//       .required(),
+//     username: Joi.string()
+//       .min(3)
+//       .required(),
+//     email: Joi.string()
+//       .email()
+//       .required()
+//   });
+//   const validation = schema.validate(req.body);
+//   if (validation.error) {
+//     return res.status(400).send(validation.error.details[0].message);
+//   }
+
+//   //User Modification
+//   (user.name = req.body.name),
+//     (user.username = req.body.username),
+//     (user.email = req.body.email);
+
+//   //res.render(userId);
+//   //res.send(user);
+//   res.redirect("/users");
+//});
 
 module.exports = router;
